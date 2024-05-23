80ef4ecd8dd38f89238b0c88d26df6e3d89ad4a7
Loren Couse <couselm@gmail.com>
2024-05-23 14:22:39 +0800

    v0.1a
    Added Hero Banner

--- src/Components/Hero/Hero.tsx
+++ src/Components/Hero/Hero.tsx
@@ -1,7 +1,20 @@
 import React from 'react'
+import image from '../../Assets/hero_image.png'
 
 export const Hero = () => {
   return (
-    <div>Hero</div>
+    <div className='flex flex-row justify-around bg-gradient-to-b from-pink-200 to-white  h-screen font-semibold'>
+        <div className="flex flex-col items-center justify-center text-left">
+            <div className='flex flex-col' >
+                <p>New Arrivals</p>
+                <p className='text-6xl leading-snug  '>New <br/> Cute <br/> Animals</p>
+                {/* <p className='text-6xl'>Cute</p>
+                <p className='text-6xl'>Animals</p> */}
+            </div>
+
+            <button className='rounded-full w-48 bg-red-600 p-3 text-white hover:bg-red-500 my-10' >Shop Now</button>
+        </div>
+        <img src={image} alt="" />
+    </div>
   )
 }
