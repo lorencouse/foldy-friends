3694483db0587d2e5b7b34da693c024981f5c027
“Loren <couselm@gmail.com>
2025-01-10 22:14:54 +0800

    update home page metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -23,6 +23,24 @@ const quicksand = Quicksand({
   subsets: ["latin"],
 });
 
+const title = "foldy friends";
+const description = "Shop origami kits and supplies.";
+
+export const metadata: Metadata = {
+  metadataBase: new URL(getURL()),
+  title: title,
+  description: description,
+  openGraph: {
+    title: title,
+    description: description,
+  },
+  icons: {
+    icon: "/favicon.ico",
+    apple: "/apple-touch-icon.png",
+    shortcut: "/favicon-16x16.png",
+  },
+};
+
 interface LayoutProps {
   children: ReactNode;
 }
