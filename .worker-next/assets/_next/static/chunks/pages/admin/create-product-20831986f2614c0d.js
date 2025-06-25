6a2d6546f406053073016461fdd715b1e66ce379
Loren Couse <couselm@gmail.com>
2025-06-25 10:38:23 -0700

    Delete .worker-next/assets directory

--- .worker-next/assets/_next/static/chunks/pages/admin/create-product-20831986f2614c0d.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[280],{8612:function(n,u,t){(window.__NEXT_P=window.__NEXT_P||[]).push(["/admin/create-product",function(){return t(1682)}])},1682:function(n,u,t){"use strict";t.r(u);var _=t(9312),c=t(2945);u.default=(0,c.Z)(_.Z)}},function(n){n.O(0,[414,90,888,774,179],function(){return n(n.s=8612)}),_N_E=n.O()}]);
